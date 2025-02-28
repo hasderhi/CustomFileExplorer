1da8bcfae81778a964cd13c49f8bd57ead182679
Tobias Kisling <hasederhi.12@gmail.com>
2025-02-28 19:29:47 +0100

    Add project files.

---
+++ CustomFileExplorer/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace CustomFileExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeViewDirectories = new System.Windows.Forms.TreeView();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.listViewFiles = new System.Windows.Forms.ListView();
+            this.contextMenuFiles = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuFiles_Open = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuFiles_Copy = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuFiles_Cut = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuFiles_Paste = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuFiles_Delete = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuFiles_Rename = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageListIcons = new System.Windows.Forms.ImageList(this.components);
+            this.contextMenuFiles_NewFolder = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.contextMenuFiles.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeViewDirectories);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnGo);
+            this.splitContainer1.Panel2.Controls.Add(this.textBoxPath);
+            this.splitContainer1.Panel2.Controls.Add(this.listViewFiles);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 215;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeViewDirectories
+            // 
+            this.treeViewDirectories.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeViewDirectories.Location = new System.Drawing.Point(0, 0);
+            this.treeViewDirectories.Name = "treeViewDirectories";
+            this.treeViewDirectories.Size = new System.Drawing.Size(215, 450);
+            this.treeViewDirectories.TabIndex = 0;
+            this.treeViewDirectories.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeViewDirectories_BeforeExpand);
+            this.treeViewDirectories.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewDirectories_AfterSelect);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(489, 0);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(92, 20);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBoxPath.Location = new System.Drawing.Point(0, 0);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(581, 20);
+            this.textBoxPath.TabIndex = 1;
+            // 
+            // listViewFiles
+            // 
+            this.listViewFiles.ContextMenuStrip = this.contextMenuFiles;
+            this.listViewFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewFiles.HideSelection = false;
+            this.listViewFiles.LabelEdit = true;
+            this.listViewFiles.Location = new System.Drawing.Point(0, 0);
+            this.listViewFiles.Name = "listViewFiles";
+            this.listViewFiles.Size = new System.Drawing.Size(581, 450);
+            this.listViewFiles.TabIndex = 0;
+            this.listViewFiles.UseCompatibleStateImageBehavior = false;
+            this.listViewFiles.View = System.Windows.Forms.View.Details;
+            // 
+            // contextMenuFiles
+            // 
+            this.contextMenuFiles.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.contextMenuFiles_Open,
+            this.contextMenuFiles_Copy,
+            this.contextMenuFiles_Cut,
+            this.contextMenuFiles_Paste,
+            this.contextMenuFiles_Delete,
+            this.contextMenuFiles_Rename,
+            this.contextMenuFiles_NewFolder});
+            this.contextMenuFiles.Name = "contextMenuFiles";
+            this.contextMenuFiles.Size = new System.Drawing.Size(181, 180);
+            this.contextMenuFiles.TabStop = true;
+            // 
+            // contextMenuFiles_Open
+            // 
+            this.contextMenuFiles_Open.Name = "contextMenuFiles_Open";
+            this.contextMenuFiles_Open.Size = new System.Drawing.Size(180, 22);
+            this.contextMenuFiles_Open.Text = "Open";
+            this.contextMenuFiles_Open.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // contextMenuFiles_Copy
+            // 
+            this.contextMenuFiles_Copy.Name = "contextMenuFiles_Copy";
+            this.contextMenuFiles_Copy.Size = new System.Drawing.Size(180, 22);
+            this.contextMenuFiles_Copy.Text = "Copy";
+            this.contextMenuFiles_Copy.Click += new System.EventHandler(this.contextMenuFiles_Copy_Click);
+            // 
+            // contextMenuFiles_Cut
+            // 
+            this.contextMenuFiles_Cut.Name = "contextMenuFiles_Cut";
+            this.contextMenuFiles_Cut.Size = new System.Drawing.Size(180, 22);
+            this.contextMenuFiles_Cut.Text = "Cut";
+            this.contextMenuFiles_Cut.Click += new System.EventHandler(this.contextMenuFiles_Cut_Click);
+            // 
+            // contextMenuFiles_Paste
+            // 
+            this.contextMenuFiles_Paste.Name = "contextMenuFiles_Paste";
+            this.contextMenuFiles_Paste.Size = new System.Drawing.Size(180, 22);
+            this.contextMenuFiles_Paste.Text = "Paste";
+            this.contextMenuFiles_Paste.Click += new System.EventHandler(this.contextMenuFiles_Paste_Click);
+            // 
+            // contextMenuFiles_Delete
+            // 
+            this.contextMenuFiles_Delete.Name = "contextMenuFiles_Delete";
+            this.contextMenuFiles_Delete.Size = new System.Drawing.Size(180, 22);
+            this.contextMenuFiles_Delete.Text = "Delete";
+            this.contextMenuFiles_Delete.Click += new System.EventHandler(this.contextMenuFiles_Delete_Click);
+            // 
+            // contextMenuFiles_Rename
+            // 
+            this.contextMenuFiles_Rename.Name = "contextMenuFiles_Rename";
+            this.contextMenuFiles_Rename.Size = new System.Drawing.Size(180, 22);
+            this.contextMenuFiles_Rename.Text = "Rename";
+            this.contextMenuFiles_Rename.Click += new System.EventHandler(this.contextMenuFiles_Rename_Click);
+            // 
+            // imageListIcons
+            // 
+            this.imageListIcons.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageListIcons.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageListIcons.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // contextMenuFiles_NewFolder
+            // 
+            this.contextMenuFiles_NewFolder.Name = "contextMenuFiles_NewFolder";
+            this.contextMenuFiles_NewFolder.Size = new System.Drawing.Size(180, 22);
+            this.contextMenuFiles_NewFolder.Text = "New Folder";
+            this.contextMenuFiles_NewFolder.Click += new System.EventHandler(this.contextMenuFiles_NewFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.contextMenuFiles.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeViewDirectories;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.ListView listViewFiles;
+        private System.Windows.Forms.ImageList imageListIcons;
+        private System.Windows.Forms.ContextMenuStrip contextMenuFiles;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuFiles_Open;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuFiles_Copy;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuFiles_Cut;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuFiles_Paste;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuFiles_Delete;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuFiles_Rename;
+        private System.Windows.Forms.ToolStripMenuItem contextMenuFiles_NewFolder;
+    }
+}
+
---
+++ CustomFileExplorer/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+
+
+
+
+
+
+namespace CustomFileExplorer
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadIcons();
+            LoadDrives();
+            InitializeListView();
+        }
+
+        private void LoadIcons()
+        {
+            imageListIcons.Images.Clear();
+
+            Icon folderIcon = ExtractFolderIcon();
+            imageListIcons.Images.Add("folder", folderIcon);
+
+            imageListIcons.Images.Add("file", SystemIcons.Application);
+
+            treeViewDirectories.ImageList = imageListIcons;
+            listViewFiles.SmallImageList = imageListIcons;
+        }
+
+
+        private Icon ExtractFolderIcon()
+        {
+            IntPtr hIcon = WinAPI.SHGetFileInfo(
+                "C:\\Windows",
+                0,
+                out WinAPI.SHFILEINFO shinfo,
+                (uint)Marshal.SizeOf(typeof(WinAPI.SHFILEINFO)),
+                WinAPI.SHGFI_ICON | WinAPI.SHGFI_LARGEICON
+            );
+
+            return Icon.FromHandle(shinfo.hIcon);
+        }
+
+
+
+        private void InitializeListView()
+        {
+            listViewFiles.View = View.Details; // Ensure it's in Details mode
+            listViewFiles.Columns.Add("Name", 250);
+            listViewFiles.Columns.Add("Size", 100);
+            listViewFiles.Columns.Add("Type", 150);
+            listViewFiles.FullRowSelect = true;
+        }
+
+        private void listViewFiles_AfterLabelEdit(object sender, LabelEditEventArgs e)
+        {
+            if (e.Label == null) return; // if no change
+
+            string oldPath = listViewFiles.Items[e.Item].Tag.ToString();
+            string newPath = Path.Combine(Path.GetDirectoryName(oldPath), e.Label);
+
+            try
+            {
+                if (Directory.Exists(oldPath))
+                    Directory.Move(oldPath, newPath);
+                else if (File.Exists(oldPath))
+                    File.Move(oldPath, newPath);
+
+                listViewFiles.Items[e.Item].Tag = newPath;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error renaming: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                e.CancelEdit = true;
+            }
+        }
+
+
+        private void LoadDrives()
+        {
+            treeViewDirectories.Nodes.Clear();
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    TreeNode node = new TreeNode(drive.Name, 0, 0) { Tag = drive.Name };
+                    node.Nodes.Add("Loading...");
+                    treeViewDirectories.Nodes.Add(node);
+                }
+            }
+        }
+
+        private void treeViewDirectories_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode node = e.Node;
+            node.Nodes.Clear();
+
+            string path = node.Tag.ToString();
+            try
+            {
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    TreeNode childNode = new TreeNode(Path.GetFileName(dir), 0, 0) { Tag = dir };
+                    childNode.Nodes.Add("Loading...");
+                    node.Nodes.Add(childNode);
+                }
+            }
+            catch { }
+        }
+
+
+        private void treeViewDirectories_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string selectedPath = e.Node.Tag.ToString();
+            textBoxPath.Text = selectedPath;
+            LoadFiles(selectedPath);
+        }
+
+        private void LoadFiles(string path)
+        {
+            listViewFiles.Items.Clear();
+
+            try
+            {
+                foreach (var dir in Directory.GetDirectories(path))
+                {
+                    ListViewItem item = new ListViewItem(Path.GetFileName(dir), 0); // Folder icon
+                    item.Tag = dir;
+                    item.SubItems.Add("");
+                    item.SubItems.Add("Folder");
+                    listViewFiles.Items.Add(item);
+                }
+
+                foreach (var file in Directory.GetFiles(path))
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    string ext = fileInfo.Extension.ToLower();
+
+                    int iconIndex = GetFileIconIndex(ext);
+                    ListViewItem item = new ListViewItem(fileInfo.Name, iconIndex);
+                    item.Tag = file;
+                    item.SubItems.Add(fileInfo.Length.ToString("N0") + " bytes");
+                    item.SubItems.Add(fileInfo.Extension.ToUpper() + " File");
+                    listViewFiles.Items.Add(item);
+                }
+            }
+            catch { }
+        }
+
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string path = textBoxPath.Text;
+            if (Directory.Exists(path))
+            {
+                LoadFiles(path);
+                HighlightTreeViewPath(path);
+            }
+            else
+            {
+                MessageBox.Show("Invalid Path!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void HighlightTreeViewPath(string path)
+        {
+            foreach (TreeNode node in treeViewDirectories.Nodes)
+            {
+                if (node.Tag.ToString().Equals(path, StringComparison.OrdinalIgnoreCase))
+                {
+                    treeViewDirectories.SelectedNode = node;
+                    node.Expand();
+                    return;
+                }
+            }
+        }
+
+
+        private Dictionary<string, int> iconCache = new Dictionary<string, int>();
+
+        private int GetFileIconIndex(string extension)
+        {
+            if (iconCache.ContainsKey(extension))
+                return iconCache[extension];
+
+            Icon icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath); // Default
+
+            string tempFile = Path.Combine(Path.GetTempPath(), "icon" + extension);
+            try
+            {
+                File.WriteAllText(tempFile, ""); // Create tempfile
+                icon = Icon.ExtractAssociatedIcon(tempFile);
+            }
+            catch { }
+            finally
+            {
+                if (File.Exists(tempFile)) File.Delete(tempFile);
+            }
+
+            imageListIcons.Images.Add(extension, icon);
+            int index = imageListIcons.Images.Count - 1;
+            iconCache[extension] = index;
+            return index;
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e) // I sadly cannot rename this without breaking the whole app apart
+        {
+            {
+                if (listViewFiles.SelectedItems.Count == 0) return;
+
+                string path = listViewFiles.SelectedItems[0].Tag.ToString();
+
+                if (Directory.Exists(path))
+                {
+                    textBoxPath.Text = path;
+                    LoadFiles(path);
+                    HighlightTreeViewPath(path);
+                }
+                else if (File.Exists(path))
+                {
+                    Process.Start(new ProcessStartInfo(path) { UseShellExecute = true });
+                }
+            }
+        }
+
+        private void contextMenuFiles_Copy_Click(object sender, EventArgs e)
+        {
+            if (listViewFiles.SelectedItems.Count == 0) return;
+            Clipboard.SetText(listViewFiles.SelectedItems[0].Tag.ToString());
+        }
+
+        private string cutFilePath = null;
+
+        private void contextMenuFiles_Cut_Click(object sender, EventArgs e)
+        {
+            if (listViewFiles.SelectedItems.Count == 0) return;
+            cutFilePath = listViewFiles.SelectedItems[0].Tag.ToString();
+        }
+
+        private void contextMenuFiles_Paste_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(cutFilePath)) return;
+
+            string destinationPath = Path.Combine(textBoxPath.Text, Path.GetFileName(cutFilePath));
+
+            try
+            {
+                File.Move(cutFilePath, destinationPath);
+                cutFilePath = null;
+                LoadFiles(textBoxPath.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error moving file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void contextMenuFiles_Delete_Click(object sender, EventArgs e)
+        {
+            if (listViewFiles.SelectedItems.Count == 0) return;
+
+            string path = listViewFiles.SelectedItems[0].Tag.ToString();
+            if (MessageBox.Show("Are you sure you want to delete this?", "Confirm",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                try
+                {
+                    if (Directory.Exists(path))
+                        Directory.Delete(path, true);
+                    else if (File.Exists(path))
+                        File.Delete(path);
+
+                    LoadFiles(textBoxPath.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error deleting file: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void contextMenuFiles_Rename_Click(object sender, EventArgs e)
+        {
+            if (listViewFiles.SelectedItems.Count == 0) return;
+            listViewFiles.SelectedItems[0].BeginEdit();
+        }
+
+        private void contextMenuFiles_NewFolder_Click(object sender, EventArgs e)
+        {
+            string currentPath = textBoxPath.Text;
+            if (string.IsNullOrEmpty(currentPath) || !Directory.Exists(currentPath))
+            {
+                MessageBox.Show("Invalid directory selected.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string newFolderPath;
+            int count = 1;
+
+            do
+            {
+                newFolderPath = Path.Combine(currentPath, $"New Folder {count}");
+                count++;
+            } while (Directory.Exists(newFolderPath));
+
+            try
+            {
+                Directory.CreateDirectory(newFolderPath);
+                LoadFiles(currentPath); // Refresh
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error creating folder: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+
+
+}
+
+public class WinAPI
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SHFILEINFO
+    {
+        public IntPtr hIcon;
+        public IntPtr iIcon;
+        public uint dwAttributes;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+        public string szDisplayName;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+        public string szTypeName;
+    };
+
+    [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+    public static extern IntPtr SHGetFileInfo(
+        string pszPath,
+        uint dwFileAttributes,
+        out SHFILEINFO psfi,
+        uint cbFileInfo,
+        uint uFlags
+    );
+
+    public const uint SHGFI_ICON = 0x000000100;
+    public const uint SHGFI_LARGEICON = 0x000000000;
+}
